8854202b596bb0ad3067a07ad2a8067ac17b83ff
Mac <mac@Macs-MacBook-Air.local>
2022-02-22 18:04:36 +0100

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col">
-      <div className="text-3xl font-bold">HELLO WORLD</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokémon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="h-16 w-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="h-16 w-16 bg-red-200" />
+      </div>
     </div>
   );
 }
