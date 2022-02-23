a1862559231166edcc8389e1954daa1b64f54e50
Mac <mac@Macs-MacBook-Air.local>
2022-02-23 17:32:51 +0100

    modifying tRPC

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,18 @@
-import * as trpcNext from "@trpc/server/adapters/next";
-// export API handler
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: () => null,
+import * as trpc from "@trpc/server";
+import { z } from "zod";
+
+const appRouter = trpc.router().query("hello", {
+  input: z
+    .object({
+      text: z.string().nullish(),
+    })
+    .nullish(),
+  resolve({ input }) {
+    return {
+      greeting: `hello ${input?.text ?? "world"}`,
+    };
+  },
 });
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
