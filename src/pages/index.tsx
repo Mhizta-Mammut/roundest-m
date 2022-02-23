a0d6dd433f5b9266fb9cd582d996c4f0036e58d2
Mac <mac@Macs-MacBook-Air.local>
2022-02-23 17:27:31 +0100

    Add pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,17 @@
 import { trpc } from "@/utils/trpc";
+import { getOptionForVotes } from "@/utils/getRandomPokemon";
 
 export default function Home() {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Hamisu" }]);
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (data) return <div>{data.greeting}</div>;
+  const [first, second] = getOptionForVotes();
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokémon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="h-16 w-16 bg-red-200" />
+        <div className="h-16 w-16 bg-red-800"> {first} </div>
         <div className="p-8">Vs</div>
-        <div className="h-16 w-16 bg-red-200" />
+        <div className="h-16 w-16 bg-red-800"> {second} </div>
       </div>
     </div>
   );
